66b8e0ca4296578c700174432c1e5da15919bbe6
Chris Alexander Pop <chrisalexanderpop@gmail.com>
2026-01-17 00:01:50 -0500

    chore: update ESLint configuration for TypeScript rules
    - Changed '@typescript-eslint/no-explicit-any' from 'error' to 'warn' to reduce strictness.
    - Added warnings for unused variables, require imports, this alias, and var requires to improve code quality and maintainability.

--- hexagon/.eslintrc.js
+++ hexagon/.eslintrc.js
@@ -20,7 +20,11 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-require-imports': 'warn',
+    '@typescript-eslint/no-this-alias': 'warn',
+    '@typescript-eslint/no-var-requires': 'warn',
     // 'prettier/prettier': ['error', { 'endOfLine': 'auto' }],
   },
 };
